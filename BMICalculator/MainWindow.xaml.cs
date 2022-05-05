2ee410d0dc2fbd8b18bc9a80fec9c8977b8b41c3
Zadok Joshua <ayegbazadok@outlook.com>
2022-05-05 14:25:41 +0100

    Configured the reset button

--- BMICalculator/MainWindow.xaml.cs
+++ BMICalculator/MainWindow.xaml.cs
@@ -32,8 +32,10 @@ namespace BMICalculator
 
         private void resetButton_Click(object sender, RoutedEventArgs e)
         {
-
-
+            resultLabel.Content = "";
+            heightTxtBox.Text = "";
+            weightTxtBox.Text = "";
+            bodyMassIndex = 0;
         }
 
         private void calculateButton_Click(object sender, RoutedEventArgs e)
@@ -57,7 +59,10 @@ namespace BMICalculator
                 MessageBox.Show("Enter Valid height please", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
-            string BMI = BMICalculation(parsedWeight, parsedHeight);
+            float BMI = BMICalculation(parsedWeight, parsedHeight);
+
+            // Using method to check condition for BMI
+            string bmiState = CheckBMIClassification(BMI);
 
             // Checking if name textbox is empty and assign it to a value
             if(nameTxtBox.Text == "")
@@ -70,15 +75,48 @@ namespace BMICalculator
             }
 
             // Display
-            resultLabel.Content = $"Hello {name}, your BMI is ";
+            resultLabel.Content = $"Hello {name}, your Body Mass Index (BMI) is {BMI}.\n";
+
+            // Display condition of BMI and current state
+            resultLabel.Content = $"";
         }
 
-        public string BMICalculation(float num1, float num2)
+        // Methods
+        public float BMICalculation(float num1, float num2)
         {
             bodyMassIndex = (num1) / (num2 * num2);
-            return bodyMassIndex.ToString();
+            return bodyMassIndex;
         }
 
-        
+        public string CheckBMIClassification(float bmiNumber)
+        {
+            if(bmiNumber < 18.5)
+            {
+                return "BMI Class: Underweight\nObesity Class: -\nDisease Risk: -\nWaist Circumference: -";
+            }
+            else if(bmiNumber >= 18.5 && bmiNumber <= 24.9)
+            {
+                return "BMI Class: Normal\nObesity Class: -\nDisease Risk: -\nWaist Circumference: -";
+            }
+            else if(bmiNumber >= 25 && bmiNumber <= 29.9)
+            {
+                return "BMI Class: Overweight\nObesity Class: -\nDisease Risk: Increased\nWaist Circumference: High";
+            }
+
+            else if (bmiNumber >= 30 && bmiNumber <= 34.9)
+            {
+                return "BMI Class: Obesity\nObesity Class: 1\nDisease Risk: High\nWaist Circumference: Very High";
+            }
+            else if (bmiNumber >= 25 && bmiNumber <= 29.9)
+            {
+                return "BMI Class: Obesity\nObesity Class: 2\nDisease Risk: Very High\nWaist Circumference: Very High";
+            }
+            else
+            {
+                return "BMI Class: Extreme Obesity\nObesity Class: 3\nDisease Risk: Extremely High\nWaist Circumference: Extremely High";
+            }
+        }
+
+
     }
 }
